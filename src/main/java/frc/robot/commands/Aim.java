2f0c8e9dfe66e9dc564308a3a2dfea0e15650ad3
SomeTrees266 <4estgx@gmail.com>
2026-01-29 19:24:14 -0600

    hello

--- src/main/java/frc/robot/commands/Aim.java
+++ src/main/java/frc/robot/commands/Aim.java
@@ -30,16 +30,15 @@ public class Aim extends Command{
     
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    
-  }
-
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
   }
+  
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
     
+  }  
 }
