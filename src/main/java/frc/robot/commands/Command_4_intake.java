ecaf8176854408a388fe29c89a0802a193affba2
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-30 15:18:12 -0600

    Intake Command

--- src/main/java/frc/robot/commands/Command_4_intake.java
+++ src/main/java/frc/robot/commands/Command_4_intake.java
@@ -1,9 +1,39 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
 public class Command_4_intake extends Command{
-  /*
-   Command code for the Intake Subsystem will go here.
+  @SuppressWarnings("PMD.UnusedPrivateField")
+  private final Intake subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
    */
+  public Command_4_intake(Intake subsystem) {
+    this.subsystem = subsystem;
+     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {subsystem.intake();}
+  
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {subsystem.intake();}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
