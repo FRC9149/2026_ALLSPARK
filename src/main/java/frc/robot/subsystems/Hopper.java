d4fd8ba1dbeaf244b2736d75c0aab0f2d085e6f0
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-29 18:15:20 -0600

    Subsystem FINSISHER

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -1,22 +1,13 @@
 package frc.robot.subsystems;
     
-import java.util.HashMap;
-
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase; 
 
 public class Hopper extends SubsystemBase{
 
-    private final SparkMax HopperMotor = new SparkMax(1, MotorType.kBrushless);
+    private final SparkMax hopperMotor = new SparkMax(1, MotorType.kBrushless);
 
     public Hopper() {
 
@@ -24,17 +15,17 @@ public class Hopper extends SubsystemBase{
         // Set the limit
         config.smartCurrentLimit(30); 
         // Apply the config to the motor
-        HopperMotor.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+        hopperMotor.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
     }
     /*
     Sets the hopper speed from -1.0 to 1.0
     */
     public void setSpeed(double speed) {
-        HopperMotor.set(speed);
+        hopperMotor.set(speed);
     }
 
     public void stop() {
-        HopperMotor.set(0);
+        hopperMotor.set(0);
     }
 
     @Override
