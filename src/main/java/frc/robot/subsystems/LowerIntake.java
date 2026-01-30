0bbc5a394b0a3d074bd533a681d6bd255d458021
SomeTrees266 <4estgx@gmail.com>
2026-01-30 16:35:24 -0600

    Sync for hugo

--- src/main/java/frc/robot/subsystems/LowerIntake.java
+++ src/main/java/frc/robot/subsystems/LowerIntake.java
@@ -1,13 +1,26 @@
 package frc.robot.subsystems;
-    
+
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.wpilibj2.command.SubsystemBase; 
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LowerIntake extends SubsystemBase{
 
-    private final SparkMax hopperMotor = new SparkMax(1, MotorType.kBrushless);
+    private final SparkMax intake2m = new SparkMax(1, MotorType.kBrushless);
+
+    
+
+    private final RelativeEncoder encoder = intake2m.getEncoder();
+    private final SparkClosedLoopController controller = intake2m.getClosedLoopController();
+
+    // Need position changed after a while
+    private static final double IN_POS = 0.0;
+    private static final double OUT_POS = 20.0;
 
     public LowerIntake() {
 
@@ -15,17 +28,27 @@ public class LowerIntake extends SubsystemBase{
         // Set the limit
         config.smartCurrentLimit(30); 
         // Apply the config to the motor
-        hopperMotor.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+        intake2m.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+
+         // PID tuning (start small)
+        config.closedLoop.pid(0.1, 0, 0);
+
+         encoder.setPosition(0); // define "in" as zero
     }
     /*
     Sets the hopper speed from -1.0 to 1.0
     */
     public void intakeDown(boolean currently_out) {
-        // if currently_out
+       controller.setReference(OUT_POS, ControlType.kPosition);
+    }
+
+     // Move intake up
+    public void intakeUp() {
+        controller.setReference(IN_POS, ControlType.kPosition);
     }
 
     public void stop() {
-        hopperMotor.set(0);
+            intake2m.stopMotor();
     }
 
     @Override
