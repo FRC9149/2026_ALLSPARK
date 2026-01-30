36489cd54baee93b702a0f0d0e08825acfb4e84a
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-30 15:48:04 -0600

    cut the warnings that I could

--- src/main/java/frc/robot/commands/Aim.java
+++ src/main/java/frc/robot/commands/Aim.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Aiming;
 
 public class Aim extends Command{
+      @SuppressWarnings("unused")
       private final Aiming subsystem;
 
   /**
