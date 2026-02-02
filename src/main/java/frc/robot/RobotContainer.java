4caea6fb3577a2a6de5cf3233830ed002ac9dd57
= <=>
2026-02-02 17:40:37 -0600

    Added hotkeys

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,8 @@ package frc.robot;
 
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.WaypointConstants;
-
+import frc.robot.commands.Command_4_intake;
+import frc.robot.commands.ShootFuel;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -21,6 +22,8 @@ import com.robocats.swerve.SwerveConfig;
 import com.robocats.swerve.SwerveSubsystem;
 import com.studica.frc.AHRS.NavXComType;
 
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LowerIntake;
 import frc.robot.subsystems.Shooter;
 
 import com.robocats.swerve.gyroscope.AhrsGyro;
@@ -28,7 +31,7 @@ import com.robocats.swerve.ModuleConfig;
 import com.robocats.controllers.Ps3;
 import com.robocats.controllers.RevGamePad;
 
-
+import frc.robot.Constants.WaypointConstants;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -45,6 +48,8 @@ public class RobotContainer {
   );
   //A changing kp so if oscilation occurs, it can be corrected
   private final Shooter shooter = new Shooter();
+  private final LowerIntake lowerIntake = new LowerIntake();
+  private final Intake intake = new Intake();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   // private final CommandXboxController m_driverController =
@@ -77,13 +82,15 @@ public class RobotContainer {
    */
   private void configureBindings() {
     Swerve.driveTo(null);
-    // RevGamePad.onX().onTrue(Swerve.driveTo(middleShootingPosition));
-    // RevGamePad.onX().onTrue(Swerve.driveTo(leftShootingposition));
-    // RevGamePad.onX().onTrue(Swerve.driveTo(rightOfLadderShootingPosition));
-    // RevGamePad.onX().onTrue(Swerve.driveTo(leftOfLadderClimbingPosition));
-    // RevGamePad.onX().onTrue(Swerve.driveTo(middleOfLadderClimbingPostion));
-    // RevGamePad.onX().onTrue(Swerve.driveTo(rightOfLadderClimbingPositionb));
-    // RevGamePad.rightTrigger().onTrue(new ShootFuel(shooter));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.middleShootingPosition));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.leftOfLadderShootingPosition));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.rightOfLadderShootingPosition));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.leftOfLadderClimbingPosition));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.middleOfLadderClimbingPostion));
+    RevGamePad.onX().onTrue(Swerve.driveTo(WaypointConstants.rightOfLadderClimbingPosition));
+    RevGamePad.onRightTrigger(1).onTrue(new ShootFuel(shooter));
+    //RevGamePad.onTriangle().onTrue(new (lowerIntake));
+    RevGamePad.onLeftTrigger(1).onTrue(new Command_4_intake(intake));
   }
 
   /* 
