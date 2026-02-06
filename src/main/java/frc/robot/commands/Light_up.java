28ad9f04a63442ff03c09cce7758ef48ac8cc986
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-02-06 16:37:09 -0600

    fkdmslam;vndfiuoageq

--- src/main/java/frc/robot/commands/Light_up.java
+++ src/main/java/frc/robot/commands/Light_up.java
@@ -1,5 +1,10 @@
 package frc.robot.commands;
 
-public class Light_up {
+import frc.robot.subsystems.LedStrip;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Light_up extends Command{
+
+    private final LedStrip subsystems;
     
 }
