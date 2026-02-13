6853553ed6c8f94a69a261a73aa5b7f03b523e85
SomeTrees266 <4estgx@gmail.com>
2026-02-13 15:15:29 -0600

    huo; a gw; nuo ;nj

--- src/main/java/frc/robot/commands/ClimbToLevel.java
+++ src/main/java/frc/robot/commands/ClimbToLevel.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Release;
 
 
 public class ClimbToLevel extends Command{
@@ -9,6 +10,7 @@ public class ClimbToLevel extends Command{
 
     private final Climber subsystem;
     private int level;
+    private final Release subsystem2;
     private boolean goingDown = false;
 
   /**
@@ -16,7 +18,8 @@ public class ClimbToLevel extends Command{
    *
    * @param subsystem The subsystem used by this command.
    */
- public ClimbToLevel(Climber subsystem, int level) { 
+ public ClimbToLevel(Climber subsystem, Release subsystem2, int level) { 
+  this.subsystem2 = subsystem2;
   this.subsystem = subsystem;
   this.level = level;
     //Use addRequirements() here to declare subsystem dependencies.
@@ -35,8 +38,16 @@ public void initialize() {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-     if (goingDown) return; // do nothing if trying to go down
+     if (goingDown) {
+      new ReleaseThenRetract(subsystem2, subsystem, level)
+
+     }
+     
+     
+     // do nothing if trying to go down
      //TODO why? why don't we just say that if we want to go down it just calls subsystem.retract();
+
+     
     subsystem.moveToHeight(level, true);
   }
 
